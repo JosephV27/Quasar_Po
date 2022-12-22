a8cc5a99c487b5445a4d8d68b0b8c10db7e4e17e
JosephV27 <joseph.valenciano@choreograph.com>
2022-12-22 16:11:09 -0600

    Add functionality to refresh the auth token every 60 minutes

--- src/pages/PopupPage.vue
+++ src/pages/PopupPage.vue
@@ -4,7 +4,6 @@
   <div>
     <input type="text" placeholder="enter email id" v-model="emailId" />
     <button type="button" @click="removeEmail">Remove</button>
-    <button type="button" @click="updateToken">Update token</button>
   </div>
 </template>
 
@@ -45,25 +44,6 @@ export default {
         .catch(function (error) {
           console.log("Something went wrong!", error);
         });
-    },
-
-    updateToken() {
-      // get google OAuth Token
-      chrome.identity.getAuthToken(
-        {
-          interactive: true,
-        },
-        function (token) {
-          if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-            return;
-          }
-
-          console.log("NEW TOKEN: " + token);
-
-          this.token = token;
-        }
-      );
     },
   },
 };
